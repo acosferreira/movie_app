4b823fe9dbeb2e1b4793439a901771adf4d8b17c
Ana Carolina Ferreira <acosferreira@gmail.com>
2018-09-02 06:41:02 -0300

    back with authentication before action

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MoviesController < ApplicationController
 
-  #before_action  :authenticate_user!,  :except => [:index, :search]
+  before_action  :authenticate_user!,  :except => [:index, :search]
   skip_before_action :verify_authenticity_token
 
   def index
